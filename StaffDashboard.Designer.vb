a8e2c267a6ec0dec150fe42cb59a09cf6bd3256c
socialkidcarbon@gmail.com <socialkidcarbon@gmail.com>
2025-12-26 01:33:44 +0800

    UI design on staff,dentist dashboard

--- StaffDashboard.Designer.vb
+++ StaffDashboard.Designer.vb
@@ -22,21 +22,26 @@ Partial Class StaffDashboard
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(StaffDashboard))
         Me.Guna2CustomGradientPanel1 = New Guna.UI2.WinForms.Guna2CustomGradientPanel()
         Me.Guna2HtmlLabel1 = New Guna.UI2.WinForms.Guna2HtmlLabel()
-        Me.ManageForm = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ManagePatientsForm = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ManageServicesForm = New System.Windows.Forms.ToolStripMenuItem()
-        Me.SystemOverviewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.PatientManagementToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AppointmentToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BillingAndPaymentsToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LogoutPictureBox1 = New Guna.UI2.WinForms.Guna2CirclePictureBox()
         Me.Guna2CustomGradientPanel1.SuspendLayout()
         Me.MenuStrip1.SuspendLayout()
+        CType(Me.LogoutPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Guna2CustomGradientPanel1
         '
+        Me.Guna2CustomGradientPanel1.Controls.Add(Me.LogoutPictureBox1)
         Me.Guna2CustomGradientPanel1.Controls.Add(Me.Guna2HtmlLabel1)
         Me.Guna2CustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Guna2CustomGradientPanel1.FillColor2 = System.Drawing.Color.Gray
+        Me.Guna2CustomGradientPanel1.FillColor3 = System.Drawing.Color.Silver
         Me.Guna2CustomGradientPanel1.Location = New System.Drawing.Point(0, 0)
         Me.Guna2CustomGradientPanel1.Name = "Guna2CustomGradientPanel1"
         Me.Guna2CustomGradientPanel1.Size = New System.Drawing.Size(1368, 124)
@@ -45,56 +50,63 @@ Partial Class StaffDashboard
         'Guna2HtmlLabel1
         '
         Me.Guna2HtmlLabel1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Guna2HtmlLabel1.AutoSize = False
         Me.Guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent
         Me.Guna2HtmlLabel1.Font = New System.Drawing.Font("Palatino Linotype", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Guna2HtmlLabel1.Location = New System.Drawing.Point(467, 38)
+        Me.Guna2HtmlLabel1.Location = New System.Drawing.Point(514, 40)
         Me.Guna2HtmlLabel1.Name = "Guna2HtmlLabel1"
-        Me.Guna2HtmlLabel1.Size = New System.Drawing.Size(371, 46)
+        Me.Guna2HtmlLabel1.Size = New System.Drawing.Size(472, 66)
         Me.Guna2HtmlLabel1.TabIndex = 21
-        Me.Guna2HtmlLabel1.Text = "Eme Dental Clinic System"
-        '
-        'ManageForm
-        '
-        Me.ManageForm.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ManagePatientsForm, Me.ManageServicesForm})
-        Me.ManageForm.Name = "ManageForm"
-        Me.ManageForm.Size = New System.Drawing.Size(212, 44)
-        Me.ManageForm.Text = "Management"
-        '
-        'ManagePatientsForm
-        '
-        Me.ManagePatientsForm.Name = "ManagePatientsForm"
-        Me.ManagePatientsForm.Size = New System.Drawing.Size(401, 44)
-        Me.ManagePatientsForm.Text = "Patient Management"
-        '
-        'ManageServicesForm
-        '
-        Me.ManageServicesForm.Name = "ManageServicesForm"
-        Me.ManageServicesForm.Size = New System.Drawing.Size(401, 44)
-        Me.ManageServicesForm.Text = "Services Management"
-        '
-        'SystemOverviewToolStripMenuItem
-        '
-        Me.SystemOverviewToolStripMenuItem.Name = "SystemOverviewToolStripMenuItem"
-        Me.SystemOverviewToolStripMenuItem.Size = New System.Drawing.Size(213, 44)
-        Me.SystemOverviewToolStripMenuItem.Text = "Appointment"
+        Me.Guna2HtmlLabel1.Text = "Dental Clinic System"
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.MenuStrip1.AutoSize = False
         Me.MenuStrip1.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.MenuStrip1.Dock = System.Windows.Forms.DockStyle.None
         Me.MenuStrip1.Font = New System.Drawing.Font("Times New Roman", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ManageForm, Me.SystemOverviewToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PatientManagementToolStripMenuItem1, Me.AppointmentToolStripMenuItem2, Me.BillingAndPaymentsToolStripMenuItem3})
         Me.MenuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow
-        Me.MenuStrip1.Location = New System.Drawing.Point(2, 126)
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 127)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(4, 2, 0, 2)
-        Me.MenuStrip1.Size = New System.Drawing.Size(279, 633)
-        Me.MenuStrip1.TabIndex = 20
+        Me.MenuStrip1.Size = New System.Drawing.Size(361, 770)
+        Me.MenuStrip1.TabIndex = 32
         Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'PatientManagementToolStripMenuItem1
+        '
+        Me.PatientManagementToolStripMenuItem1.Name = "PatientManagementToolStripMenuItem1"
+        Me.PatientManagementToolStripMenuItem1.Size = New System.Drawing.Size(319, 44)
+        Me.PatientManagementToolStripMenuItem1.Text = "Patient Management"
+        '
+        'AppointmentToolStripMenuItem2
+        '
+        Me.AppointmentToolStripMenuItem2.Name = "AppointmentToolStripMenuItem2"
+        Me.AppointmentToolStripMenuItem2.Size = New System.Drawing.Size(213, 44)
+        Me.AppointmentToolStripMenuItem2.Text = "Appointment"
+        '
+        'BillingAndPaymentsToolStripMenuItem3
+        '
+        Me.BillingAndPaymentsToolStripMenuItem3.Name = "BillingAndPaymentsToolStripMenuItem3"
+        Me.BillingAndPaymentsToolStripMenuItem3.Size = New System.Drawing.Size(330, 44)
+        Me.BillingAndPaymentsToolStripMenuItem3.Text = "Billing and Payments"
+        '
+        'LogoutPictureBox1
+        '
+        Me.LogoutPictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LogoutPictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.LogoutPictureBox1.FillColor = System.Drawing.Color.Transparent
+        Me.LogoutPictureBox1.Image = CType(resources.GetObject("LogoutPictureBox1.Image"), System.Drawing.Image)
+        Me.LogoutPictureBox1.ImageRotate = 0!
+        Me.LogoutPictureBox1.Location = New System.Drawing.Point(1268, 29)
+        Me.LogoutPictureBox1.Name = "LogoutPictureBox1"
+        Me.LogoutPictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.LogoutPictureBox1.Size = New System.Drawing.Size(64, 64)
+        Me.LogoutPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.LogoutPictureBox1.TabIndex = 33
+        Me.LogoutPictureBox1.TabStop = False
         '
         'StaffDashboard
         '
@@ -107,25 +119,31 @@ Partial Class StaffDashboard
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "StaffDashboard"
         Me.Guna2CustomGradientPanel1.ResumeLayout(False)
-        Me.Guna2CustomGradientPanel1.PerformLayout()
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        CType(Me.LogoutPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents SystemOverviewToolStripMenuItem As ToolStripMenuItem
+
+    Private Sub ManageForm_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs)
+
+    End Sub
+
+    Private Sub MenuStrip1_ItemClicked_1(sender As Object, e As ToolStripItemClickedEventArgs)
+
+    End Sub
+
     Friend WithEvents Guna2CustomGradientPanel1 As Guna.UI2.WinForms.Guna2CustomGradientPanel
     Friend WithEvents Guna2HtmlLabel1 As Guna.UI2.WinForms.Guna2HtmlLabel
-    Friend WithEvents ManageForm As ToolStripMenuItem
-    Friend WithEvents ManagePatientsForm As ToolStripMenuItem
-    Friend WithEvents ManageServicesForm As ToolStripMenuItem
-    Friend WithEvents SystemOverviewToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents MenuStrip1 As MenuStrip
-
-    Private Sub ManageForm_Click(sender As Object, e As EventArgs) Handles ManageForm.Click
-
-    End Sub
-
-    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStrip1.ItemClicked
-
-    End Sub
+    Friend WithEvents PatientManagementToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents AppointmentToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents BillingAndPaymentsToolStripMenuItem3 As ToolStripMenuItem
+    Friend WithEvents LogoutPictureBox1 As Guna.UI2.WinForms.Guna2CirclePictureBox
 End Class
