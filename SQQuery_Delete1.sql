c1f3194286ce6f5501ab969647e56155be37990a
socialkidcarbon@gmail.com <socialkidcarbon@gmail.com>
2026-03-17 09:34:02 +0800

    sql file

---
+++ SQQuery_Delete1.sql
@@ -0,0 +1,19 @@
+﻿-- Step 1: Clear Junction and Dependent Tables (Child Tables)
+DELETE FROM appointmentServices;
+DELETE FROM TreatmentRecords;
+DELETE FROM StockTransactions;
+DELETE FROM UserSessions;
+DELETE FROM AuditTrail;
+DELETE FROM DentistAvailability;
+
+-- Step 2: Clear Tables with Foreign Key dependencies (Intermediate)
+DELETE FROM appointments;
+DELETE FROM Receipts;
+DELETE FROM ItemManagement;
+
+-- Step 3: Clear Primary Data (Parent Tables)
+DELETE FROM Patients;
+DELETE FROM Services;
+DELETE FROM Categories;
+DELETE FROM Suppliers;
+DELETE FROM users;
